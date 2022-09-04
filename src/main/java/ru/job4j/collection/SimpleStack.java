317fda01c68c1417f1f61f94d2aa0696cfff8a55
Kuzmich36rus <sledak-vrn@yandex.ru>
2022-09-04 22:32:03 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474]

---
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleStack<T> {
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    private int size = 0;
+
+    public T pop() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        size--;
+        return linked.deleteFirst();
+    }
+
+    public void push(T value) {
+        linked.addFirst(value);
+        size++;
+    }
+}
